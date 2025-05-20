9d60fcee5a5011a88c777a40ed815025a6704aa2
BryanPhand <bryan_a.r@hotmail.com>
2025-05-20 00:50:51 -0300

    Commit contendo os arquivos do desafio POO da DIO

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # desafio-poo
+# Projeto iPhone - UML
+
+Este projeto modela as funcionalidades principais do iPhone lançado em 2007, incluindo:
+
+- Reprodutor Musical
+- Aparelho Telefônico
+- Navegador de Internet
+
+---
+
+## Diagrama UML do Componente iPhone
+
+```mermaid
+classDiagram
+    class ReprodutorMusical {
+        +tocar()
+        +pausar()
+        +selecionarMusica(musica: String)
+    }
+
+    class AparelhoTelefonico {
+        +ligar(numero: String)
+        +atender()
+        +iniciarCorreioVoz()
+    }
+
+    class NavegadorInternet {
+        +exibirPagina(url: String)
+        +adicionarNovaAba()
+        +atualizarPagina()
+    }
+
+    class iPhone {
+        +tocar()
+        +pausar()
+        +selecionarMusica(musica: String)
+        +ligar(numero: String)
+        +atender()
+        +iniciarCorreioVoz()
+        +exibirPagina(url: String)
+        +adicionarNovaAba()
+        +atualizarPagina()
+    }
+
+    iPhone ..|> ReprodutorMusical
+    iPhone ..|> AparelhoTelefonico
+    iPhone ..|> NavegadorInternet
---
+++ src/Dispositivo/Iphone.java
@@ -0,0 +1,70 @@
+package Dispositivo;
+
+import Funcionalidades.AparelhoTelefonico;
+import Funcionalidades.NavegadorInternet;
+import Funcionalidades.ReprodutorMusical;
+
+public class Iphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada.");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Música selecionada: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para " + numero + "...");
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Ligação atendida.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz...");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada.");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada.");
+    }
+
+    // Testes
+    public static void main(String[] args) {
+        Iphone meuIphone = new Iphone();
+
+        meuIphone.selecionarMusica("Imagine - John Lennon");
+        meuIphone.tocar();
+        meuIphone.pausar();
+
+        meuIphone.ligar("1234-5678");
+        meuIphone.atender();
+        meuIphone.iniciarCorreioVoz();
+
+        meuIphone.exibirPagina("https://www.apple.com");
+        meuIphone.adicionarNovaAba();
+        meuIphone.atualizarPagina();
+    }
+}
